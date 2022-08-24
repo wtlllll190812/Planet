ad0b7c577f4e4d0d8916bcbfe40dc0d31742dffd
wtlllll <3200102777@zju.edu.cn>
2022-08-24 16:43:48 +0800

    添加游戏状态机

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Linq;
 using System.Collections;
 using Sirenix.OdinInspector;
 using System.Collections.Generic;
@@ -7,10 +8,32 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameState currentState;
+    public List<Planet> planetList;
+    public Planet currentPlanet;
 
     public void Awake()
     {
         instance = this;
+    }
+
+    [Button("SetState")]
+    public void SetState(GameState state,Planet planet)
+    {
+        switch (state)
+        {
+            case GameState.GlobalView:
+                CameraControl.instance.SetTarget(new CameraData(Sun.instance.transform,10));
+                break;
+            case GameState.PlanetView:
+                planet.CameraFollow();
+                break;
+            case GameState.Editor:
+                planet.SetEditorView();
+                break;
+            default:
+                break;
+        }
+        currentState = state;
     }
 }
 public enum GameState
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -8,16 +8,33 @@ public class Planet : MonoBehaviour
     public float size;
     public float rotationSpeed;
     public float revolutionSpeed;
+    public bool isSun;
+    public Transform plantTr;
+
+    public void Start()
+    {
+        if(transform.childCount!=0)
+            plantTr = transform.GetChild(0);
+    }
 
     [Button("MoveCamera")]
-    public void CameraMove()
+    public void CameraFollow()
     {
-        CameraControl.instance.SetTarget(new CameraData(transform,size));
+        if(plantTr)
+            CameraControl.instance.SetTarget(new CameraData(plantTr, size));
+    }
+
+    public void SetEditorView()
+    {
+        CameraControl.instance.SetTarget(new CameraData(transform, size));
     }
 
     public void FixedUpdate()
     {
-        transform.RotateAround(Sun.instance.transform.position, Vector3.forward, Time.deltaTime*revolutionSpeed);
-        transform.Rotate(Vector3.forward,Time.deltaTime*rotationSpeed);
+        if(GameManager.instance.currentState!=GameState.Editor)
+        {
+            plantTr.RotateAround(Sun.instance.transform.position, Vector3.forward, Time.deltaTime * revolutionSpeed);
+            plantTr?.Rotate(Vector3.forward, Time.deltaTime * rotationSpeed,Space.Self);
+        }
     }
 }
